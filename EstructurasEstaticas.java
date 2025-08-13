69f5ee7ddaf7e565d19f0af8f91172e6f5152839
DagoDes <gato931104@gmail.com>
2025-08-13 16:39:11 -0500

    añadi archivos

---
+++ EstructurasEstaticas.java
@@ -0,0 +1,34 @@
+public class EstructurasEstaticas {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 11;
+        //int a = 10, b = 11;
+        int suma = a + b;
+        int resta = a - b;
+        long c = 30000000000000L;
+        float divs = a / b;
+        // Optional divs = a / b;  Si la division es a / 0
+        char bo = 'A';
+        String word = "Hola";
+        char f = 'H';
+        char q = 'o';
+        char t = 'l';
+        char r = 'a';
+        String sume = f + q + t + r;
+        Boolean w = true;
+        Boolean q = false;
+
+        // Ejemplos
+
+        String name = "Samuel";
+        int edad = 18;
+        Boolean soltero = false;
+        char primeraletraname = 'S';
+        float ganancias = 20 / 3;
+
+
+
+    } 
+}
+
---
+++ EstructurasEstaticas.py
@@ -0,0 +1,26 @@
+a = 10
+b = 11
+a, b = 10, 11
+
+suma = a + b
+resta = a - b
+
+word = "hola"
+for i in word:
+    print(i)
+
+p = 'B'
+q = 'u'
+r = 'r'
+s = 'r'
+t = 'a'
+
+print(p + q + r + s + t)
+
+# jemplos
+
+s = "Samuel"
+i = 18 
+u = False
+h = 0.45
+j = 'd'
