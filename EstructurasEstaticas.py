a4973d9d7a0ff817313dfa9ceaa55df4985b59ae
Oxsitoo <fsgarciah@academia.usbbog.edu.co>
2025-08-27 01:25:08 +0000

    Ejercicios python terminados

--- EstructurasEstaticas.py
+++ EstructurasEstaticas.py
@@ -19,8 +19,33 @@ print(p + q + r + s + t)
 
 # jemplos
 
-s = "Samuel"
-i = 18 
-u = False
-h = 0.45
-j = 'd'
+string1 = "Hola"
+string2 = "Python"
+string3 = "ChatGPT"
+string4 = "12345"
+string5 = "¡Bienvenido!"
+
+num1 = 10
+num2 = -25
+num3 = 0
+num4 = 999999
+num5 = 42
+
+bool1 = True
+bool2 = False
+bool3 = (5 > 3)   # True
+bool4 = (10 == 5) # False
+bool5 = (7 != 2)  # True
+
+float1 = 3.14
+float2 = -0.5
+float3 = 2.718
+float4 = 1000.0001
+float5 = 0.0
+
+
+caracter1 = 'a'
+caracter2 = 'Z'
+caracter3 = '9'
+caracter4 = '$'
+caracter5 = 'ñ'
